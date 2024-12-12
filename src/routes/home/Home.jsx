4844dd49d9150ec27e08e9b058faf73efb2cf164
LillKonst <konstlillkristine@gmail.com>
2024-12-12 12:53:56 +0100

    added about component

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,11 +1,13 @@
 import Archive from "../../components/archive/Archive";
 import MainImage from "../../components/mainImg/MainImg";
+import About from "../../components/about/About";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <MainImage />
       <Archive />
+      <About />
     </div>
   );
 }

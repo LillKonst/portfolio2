2277bc10c13d267d74c6368a265562afe1720a48
LillKonst <konstlillkristine@gmail.com>
2024-12-13 11:54:17 +0100

    added new cards and layout for archive

---
+++ src/components/archive/cardCollection/CardCollection.jsx
@@ -0,0 +1,13 @@
+import HolidazeCard from "../holidaze-card/HolidazeCard";
+import EssentailsCard from "../essentials-card/EssentialsCard";
+import BiddingsCard from "../biddings-card/BiddingsCard";
+
+export default function CardCollection() {
+  return (
+    <div className="grid grid-cols-6 gap-8">
+      <HolidazeCard />
+      <EssentailsCard />
+      <BiddingsCard />
+    </div>
+  );
+}
